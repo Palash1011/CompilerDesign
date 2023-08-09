b826ad6555e9a70f80d1a114dcd217cc8219f8e0
Palash <124863168+Palash1011@users.noreply.github.com>
2023-08-09 10:38:04 +0530

    Update Follow.c

--- Follow.c
+++ Follow.c
@@ -1,104 +1,78 @@
 #include<stdio.h> 
-#include<ctype.h>
 #include<string.h>
-int limit, x = 0;
-char production[10][10], array[10];
- 
-void find_first(char ch);
-void find_follow(char ch);
-void Array_Manipulation(char ch);
- 
+int n,m=0,p,i=0,j=0;
+char a[10][10],followResult[10];
+void follow(char c);
+void first(char c);
+void addToResult(char);
 int main()
 {
-      int count;
-      char option, ch;
-      printf("\nEnter Total Number of Productions:\t");
-      scanf("%d", &limit);
-      for(count = 0; count < limit; count++)
-      {
-            printf("\nValue of Production Number [%d]:\t", count + 1);
-            scanf("%s", production[count]);
-      }
-      do
-      {
-            x = 0;
-            printf("\nEnter production Value to Find Follow:\t");
-            scanf(" %c", &ch);
-            find_follow(ch);
-            printf("\nFollow Value of %c:\t{ ", ch);
-            for(count = 0; count < x; count++)
-            {
-                  printf("%c ", array[count]);
-            }
-            printf("}\n");
-            printf("To Continue, Press Y:\t");
-            scanf(" %c", &option);
-      }while(option == 'y' || option == 'Y');
-      return 0;
+ int i;
+ int choice;
+ char c,ch;
+ printf("Enter the no.of productions: ");
+scanf("%d", &n);
+ printf(" Enter %d productions\nProduction with multiple terms should be give as separate productions \n", n);
+ for(i=0;i<n;i++)
+  scanf("%s%c",a[i],&ch);
+    // gets(a[i]);
+ do
+ {
+  m=0;
+  printf("Find FOLLOW of -->");
+  scanf(" %c",&c);
+  follow(c);
+  printf("FOLLOW(%c) = { ",c);
+  for(i=0;i<m;i++)
+   printf("%c ",followResult[i]);
+  printf(" }\n");
+  printf("Do you want to continue(Press 1 to continue....)?");
+ scanf("%d%c",&choice,&ch);
+ }
+ while(choice==1);
 }
- 
-void find_follow(char ch)
+void follow(char c)
 {
-      int i, j;
-      int length = strlen(production[i]);
-      if(production[0][0] == ch)
-      {
-            Array_Manipulation('$');
-      }
-      for(i = 0; i < limit; i++)
-      {
-            for(j = 2; j < length; j++)
-            {
-                  if(production[i][j] == ch)
-                  {
-                        if(production[i][j + 1] != '\0')
-                        {
-                              find_first(production[i][j + 1]);
-                        }
-                        if(production[i][j + 1] == '\0' && ch != production[i][0])
-                        {     
-                              find_follow(production[i][0]);
-                        }
-                  }
-            }
-      }
+    if(a[0][0]==c)addToResult('$');
+ for(i=0;i<n;i++)
+ {
+  for(j=2;j<strlen(a[i]);j++)
+  {
+   if(a[i][j]==c)
+   {
+    if(a[i][j+1]!='\0')first(a[i][j+1]);
+    if(a[i][j+1]=='\0'&&c!=a[i][0])
+     follow(a[i][0]);
+   }
+  }
+ }
 }
- 
-void find_first(char ch)
+void first(char c)
 {
-      int i, k;
-      if(!(isupper(ch)))
-      {
-            Array_Manipulation(ch);
-      }
-      for(k = 0; k < limit; k++)
-      {
-            if(production[k][0] == ch)
-            {
-                  if(production[k][2] == '$') 
-                  {
-                        find_follow(production[i][0]);
-                  }
-                  else if(islower(production[k][2]))
-                  {
-                        Array_Manipulation(production[k][2]);
-                  }
-                  else 
-                  {
-                        find_first(production[k][2]);
-                  }
-            }
-      }
+      int k;
+                 if(!(isupper(c)))
+                     //f[m++]=c;
+                     addToResult(c);
+                 for(k=0;k<n;k++)
+                 {
+                 if(a[k][0]==c)
+                 {
+                 if(a[k][2]=='$') follow(a[i][0]);
+                 else if(islower(a[k][2]))
+                     //f[m++]=a[k][2];
+                     addToResult(a[k][2]);
+                 else first(a[k][2]);
+                 }
+                 }
 }
- 
-void Array_Manipulation(char ch)
+void  addToResult(char c)
 {
-      int count;
-      for(count = 0; count <= x; count++)
-      {
-            if(array[count] == ch)
-            {
-                  return;
+    int i;
+    for( i=0;i<=m;i++)
+        if(followResult[i]==c)
+            return;
+   followResult[m++]=c;
+}
             }
       }
       array[x++] = ch;
